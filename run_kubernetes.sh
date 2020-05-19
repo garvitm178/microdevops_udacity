fac5e9ea1c9a25d96e831cb334c61ab3efbcebab
Garvit Mehta <35095197+garvitm178@users.noreply.github.com>
2020-05-19 19:32:05 +0530

    adding files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="garvitmehta/project-ml-microservice-kubernetes"
+tag_name="microdocker"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $tag_name --image=$dockerpath --port=80 --labels app=$tag_name
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $tag_name 8000:80
